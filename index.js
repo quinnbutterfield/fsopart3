c179a641d73bf09d22a58dd8e31aaf5f5519fba8
quinnbutterfield <quinnob66@gmail.com>
2020-07-08 18:54:41 -0700

    part 3a exercises completed

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const { response } = require('express')
+const morgan = require('morgan')
+const { json } = require('express')
+//const { response } = require('express')
 const app = express()
 
+
+morgan.token('body', function (req, res) {
+    return JSON.stringify(req.body)
+})
+
 app.use(express.json())
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time :body'))
 
 let persons = [
 
@@ -24,6 +33,8 @@ let persons = [
 
 ]
 
+
+
 //Show overview
 app.get('/info', (req, res) => {
     res.send(
@@ -41,8 +52,8 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
-    
-    if(person){
+
+    if (person) {
         res.json(person)
     } else {
         res.status(404).end()
@@ -63,13 +74,41 @@ const generateId = () => {
 
 //Add a person
 app.post('/api/persons', (req, res) => {
+
     const body = req.body
-    if(!body.content) {
+    if (!body.name) {
         return res.status(400).json({
-            error: 'content missing'
+            error: 'name is missing'
         })
     }
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'number is missing'
+        })
+    }
+    if (persons.some(p => p.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
 })
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
