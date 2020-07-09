015a3c1db3b87f38086c64de7c5f8fca3362a6f2
quinnbutterfield <quinnob66@gmail.com>
2020-07-09 14:50:46 -0700

    maybe it will work

--- index.js
+++ index.js
@@ -104,13 +104,13 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-/*
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
-*/
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
